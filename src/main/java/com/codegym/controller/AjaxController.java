16c6572748892e09438484d038a061dc769d1213
phantiensy <phantiensy195@gmail.com>
2018-08-29 18:18:07 +0700

    AjaxController

---
+++ src/main/java/com/codegym/controller/AjaxController.java
@@ -0,0 +1,37 @@
+package com.codegym.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+public class AjaxController {
+    @GetMapping("/")
+    public String showIndex(Model model) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        model.addAttribute("timeNow", dtf.format(now));
+        return "index";
+    }
+
+    @PostMapping(value = "/getTimezone")
+    @ResponseBody
+    public String createSmartPhone(@RequestParam("val") String timezone) {
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        df.setTimeZone(TimeZone.getTimeZone(timezone));
+        String currentDate = df.format(date);
+        return currentDate;
+    }
+}
